88fa560b3c6520f669b032e80bfb0c837a9de976
lmint1 <lemint.dev@gmail.com>
2021-05-12 23:34:38 -0300

    TouchPoint toJson and fromJson

--- lib/models/touch_point.dart
+++ lib/models/touch_point.dart
@@ -6,6 +6,20 @@ class TouchPoint {
 
   TouchPoint(this.color, this.offset);
 
+  TouchPoint.fromJson(Map<String, dynamic> json):
+        color = Color(json["color"]),
+        offset = _fromValueList(json["off"]);
+
+  Map<String, dynamic> toJson() => {
+    'color': color.value,
+    'offset': [offset.dx, offset.dy],
+  };
+
+  static Offset _fromValueList(List<double> array) {
+    try { return Offset(array[0], array[1]); }
+    on Exception { return null; }
+  }
+
   bool hasOffset() => offset != null;
   bool hasNotOffset() => offset == null;
 }
